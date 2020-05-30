33a0dbf3c5a77a51a11d198a833efc12138589d6
FerAhuatzin <64028335+FerAhuatzin@users.noreply.github.com>
2020-05-30 12:34:17 -0500

    Fer
    Ya no se conglomeran las preguntas, ahora cada que pones una respuesta buena solo sale una a la vez

--- Trivia.js
+++ Trivia.js
@@ -1,8 +1,7 @@
 var espacio = $('#espacio')
 espacio.hide()
-function Jugar(array) {
+function Jugar() {
     var array = []
-    console.log(array)
     var jugar = $('#Jugar')
     jugar.hide()
     espacio.show()
@@ -39,6 +38,7 @@ function Jugar(array) {
         p30: "Tiene dos hermanas",
         p31: "Salió con el papá de una de las novias de Ross",
         p32: "Su frase habitual es NOO",
+        p33: "De joven se operó la nariz",
         p34: "Fué dama de honor en la boda de su exnovio con su mejor amiga",
         p35: "La más joven del grupo",
         p36: "Salió con Joey por un tiempo",
@@ -130,301 +130,203 @@ function Jugar(array) {
 
     var preguntaAleatoria = Math.floor(Math.random() * (120) + 1)
     console.log(preguntaAleatoria)
-
-
-    pregunta(array, preguntaAleatoria, 1, preguntas.p1)
-    pregunta(array, preguntaAleatoria, 2, preguntas.p2)
-    pregunta(array, preguntaAleatoria, 3, preguntas.p3)
-    pregunta(array, preguntaAleatoria, 4, preguntas.p4)
-    pregunta(array, preguntaAleatoria, 5, preguntas.p5)
-    pregunta(array, preguntaAleatoria, 6, preguntas.p6)
-    pregunta(array, preguntaAleatoria, 7, preguntas.p7)
-    pregunta(array, preguntaAleatoria, 8, preguntas.p8)
-    pregunta(array, preguntaAleatoria, 9, preguntas.p9)
-    pregunta(array, preguntaAleatoria, 10, preguntas.p10)
-    pregunta(array, preguntaAleatoria, 11, preguntas.p11)
-    pregunta(array, preguntaAleatoria, 12, preguntas.p12)
-    pregunta(array, preguntaAleatoria, 13, preguntas.p13)
-    pregunta(array, preguntaAleatoria, 14, preguntas.p14)
-    pregunta(array, preguntaAleatoria, 15, preguntas.p15)
-    pregunta(array, preguntaAleatoria, 16, preguntas.p16)
-    pregunta(array, preguntaAleatoria, 17, preguntas.p17)
-    pregunta(array, preguntaAleatoria, 18, preguntas.p18)
-    pregunta(array, preguntaAleatoria, 19, preguntas.p19)
-    pregunta(array, preguntaAleatoria, 20, preguntas.p20)
-    pregunta(array, preguntaAleatoria, 21, preguntas.p21)
-    pregunta(array, preguntaAleatoria, 22, preguntas.p22)
-    pregunta(array, preguntaAleatoria, 23, preguntas.p23)
-    pregunta(array, preguntaAleatoria, 24, preguntas.p24)
-    pregunta(array, preguntaAleatoria, 25, preguntas.p25)
-    pregunta(array, preguntaAleatoria, 26, preguntas.p26)
-    pregunta(array, preguntaAleatoria, 27, preguntas.p27)
-    pregunta(array, preguntaAleatoria, 28, preguntas.p28)
-    pregunta(array, preguntaAleatoria, 29, preguntas.p29)
-    pregunta(array, preguntaAleatoria, 30, preguntas.p30)
-    pregunta(array, preguntaAleatoria, 31, preguntas.p31)
-    pregunta(array, preguntaAleatoria, 32, preguntas.p32)
-    pregunta(array, preguntaAleatoria, 33, preguntas.p33)
-    pregunta(array, preguntaAleatoria, 34, preguntas.p34)
-    pregunta(array, preguntaAleatoria, 35, preguntas.p35)
-    pregunta(array, preguntaAleatoria, 36, preguntas.p36)
-    pregunta(array, preguntaAleatoria, 37, preguntas.p37)
-    pregunta(array, preguntaAleatoria, 38, preguntas.p38)
-    pregunta(array, preguntaAleatoria, 39, preguntas.p39)
-    pregunta(array, preguntaAleatoria, 40, preguntas.p40)
-    pregunta(array, preguntaAleatoria, 41, preguntas.p41)
-    pregunta(array, preguntaAleatoria, 42, preguntas.p42)
-    pregunta(array, preguntaAleatoria, 43, preguntas.p43)
-    pregunta(array, preguntaAleatoria, 44, preguntas.p44)
-    pregunta(array, preguntaAleatoria, 45, preguntas.p45)
-    pregunta(array, preguntaAleatoria, 46, preguntas.p46)
-    pregunta(array, preguntaAleatoria, 47, preguntas.p47)
-    pregunta(array, preguntaAleatoria, 48, preguntas.p48)
-    pregunta(array, preguntaAleatoria, 49, preguntas.p49)
-    pregunta(array, preguntaAleatoria, 50, preguntas.p50)
-    pregunta(array, preguntaAleatoria, 51, preguntas.p51)
-    pregunta(array, preguntaAleatoria, 52, preguntas.p52)
-    pregunta(array, preguntaAleatoria, 53, preguntas.p53)
-    pregunta(array, preguntaAleatoria, 54, preguntas.p54)
-    pregunta(array, preguntaAleatoria, 55, preguntas.p55)
-    pregunta(array, preguntaAleatoria, 56, preguntas.p56)
-    pregunta(array, preguntaAleatoria, 57, preguntas.p57)
-    pregunta(array, preguntaAleatoria, 58, preguntas.p58)
-    pregunta(array, preguntaAleatoria, 59, preguntas.p59)
-    pregunta(array, preguntaAleatoria, 60, preguntas.p60)
-    pregunta(array, preguntaAleatoria, 61, preguntas.p61)
-    pregunta(array, preguntaAleatoria, 62, preguntas.p62)
-    pregunta(array, preguntaAleatoria, 63, preguntas.p63)
-    pregunta(array, preguntaAleatoria, 64, preguntas.p64)
-    pregunta(array, preguntaAleatoria, 65, preguntas.p65)
-    pregunta(array, preguntaAleatoria, 66, preguntas.p66)
-    pregunta(array, preguntaAleatoria, 67, preguntas.p67)
-    pregunta(array, preguntaAleatoria, 68, preguntas.p68)
-    pregunta(array, preguntaAleatoria, 69, preguntas.p69)
-    pregunta(array, preguntaAleatoria, 70, preguntas.p70)
-    pregunta(array, preguntaAleatoria, 71, preguntas.p71)
-    pregunta(array, preguntaAleatoria, 72, preguntas.p72)
-    pregunta(array, preguntaAleatoria, 73, preguntas.p73)
-    pregunta(array, preguntaAleatoria, 74, preguntas.p74)
-    pregunta(array, preguntaAleatoria, 75, preguntas.p75)
-    pregunta(array, preguntaAleatoria, 76, preguntas.p76)
-    pregunta(array, preguntaAleatoria, 77, preguntas.p77)
-    pregunta(array, preguntaAleatoria, 78, preguntas.p78)
-    pregunta(array, preguntaAleatoria, 79, preguntas.p79)
-    pregunta(array, preguntaAleatoria, 80, preguntas.p80)
-    pregunta(array, preguntaAleatoria, 81, preguntas.p81)
-    pregunta(array, preguntaAleatoria, 82, preguntas.p82)
-    pregunta(array, preguntaAleatoria, 83, preguntas.p83)
-    pregunta(array, preguntaAleatoria, 84, preguntas.p84)
-    pregunta(array, preguntaAleatoria, 85, preguntas.p85)
-    pregunta(array, preguntaAleatoria, 86, preguntas.p86)
-    pregunta(array, preguntaAleatoria, 87, preguntas.p87)
-    pregunta(array, preguntaAleatoria, 88, preguntas.p88)
-    pregunta(array, preguntaAleatoria, 89, preguntas.p89)
-    pregunta(array, preguntaAleatoria, 90, preguntas.p90)
-    pregunta(array, preguntaAleatoria, 91, preguntas.p91)
-    pregunta(array, preguntaAleatoria, 92, preguntas.p92)
-    pregunta(array, preguntaAleatoria, 93, preguntas.p93)
-    pregunta(array, preguntaAleatoria, 94, preguntas.p94)
-    pregunta(array, preguntaAleatoria, 95, preguntas.p95)
-    pregunta(array, preguntaAleatoria, 96, preguntas.p96)
-    pregunta(array, preguntaAleatoria, 97, preguntas.p97)
-    pregunta(array, preguntaAleatoria, 98, preguntas.p98)
-    pregunta(array, preguntaAleatoria, 99, preguntas.p99)
-    pregunta(array, preguntaAleatoria, 100, preguntas.p100)
-    pregunta(array, preguntaAleatoria, 101, preguntas.p101)
-    pregunta(array, preguntaAleatoria, 102, preguntas.p102)
-    pregunta(array, preguntaAleatoria, 103, preguntas.p103)
-    pregunta(array, preguntaAleatoria, 104, preguntas.p104)
-    pregunta(array, preguntaAleatoria, 105, preguntas.p105)
-    pregunta(array, preguntaAleatoria, 106, preguntas.p106)
-    pregunta(array, preguntaAleatoria, 107, preguntas.p107)
-    pregunta(array, preguntaAleatoria, 108, preguntas.p108)
-    pregunta(array, preguntaAleatoria, 109, preguntas.p109)
-    pregunta(array, preguntaAleatoria, 110, preguntas.p110)
-    pregunta(array, preguntaAleatoria, 111, preguntas.p111)
-    pregunta(array, preguntaAleatoria, 112, preguntas.p112)
-    pregunta(array, preguntaAleatoria, 113, preguntas.p113)
-    pregunta(array, preguntaAleatoria, 114, preguntas.p114)
-    pregunta(array, preguntaAleatoria, 115, preguntas.p115)
-    pregunta(array, preguntaAleatoria, 116, preguntas.p116)
-    pregunta(array, preguntaAleatoria, 117, preguntas.p117)
-    pregunta(array, preguntaAleatoria, 118, preguntas.p118)
-    pregunta(array, preguntaAleatoria, 119, preguntas.p119)
-    pregunta(array, preguntaAleatoria, 120, preguntas.p120)
+    
+    var contador = 1;
+    
+    for (const key in preguntas) {
+        if (preguntas.hasOwnProperty(key)) {
+            const element = preguntas[key];
+            pregunta(array, preguntaAleatoria, contador, element)
+            contador++
+        }
+    }
+    
 
     if (preguntaAleatoria <= 20 && preguntaAleatoria >= 1) {
         document.getElementById('Ross').addEventListener('click', function () {
             console.log("Respuesta correcta")
             preguntaAleatoria = 0
             console.log(preguntaAleatoria)
+            array.pop(0)
+            console.log(array)
+            remover()
             Jugar()
-            borrar()
+            
         })
         document.getElementById('Rachel').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Joey').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Chandler').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Monica').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Phoebe').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
     }
-    if (preguntaAleatoria <= 40 && preguntaAleatoria >= 20) {
+    if (preguntaAleatoria <= 40 && preguntaAleatoria >= 21) {
         document.getElementById('Rachel').addEventListener('click', function () {
             console.log("Respuesta correcta")
             preguntaAleatoria = 0
             console.log(preguntaAleatoria)
+            array.pop(0)
+            remover()
             Jugar()
-            borrar()
+            
         })
         document.getElementById('Ross').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Joey').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Chandler').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Monica').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Phoebe').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
     }
-    if (preguntaAleatoria <= 60 && preguntaAleatoria >= 40) {
+    if (preguntaAleatoria <= 60 && preguntaAleatoria >= 41) {
         document.getElementById('Joey').addEventListener('click', function () {
             console.log("Respuesta correcta")
             preguntaAleatoria = 0
             console.log(preguntaAleatoria)
+            array.pop(0)
+            remover()
             Jugar()
-            borrar()
+            
         })
         document.getElementById('Rachel').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Ross').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Chandler').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Monica').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Phoebe').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
     }
-    if (preguntaAleatoria <= 80 && preguntaAleatoria >= 60) {
+    if (preguntaAleatoria <= 80 && preguntaAleatoria >= 61) {
         document.getElementById('Chandler').addEventListener('click', function () {
             console.log("Respuesta correcta")
             preguntaAleatoria = 0
             console.log(preguntaAleatoria)
+            array.pop(0)
+            remover()
             Jugar()
-            borrar()
+            
         })
         document.getElementById('Rachel').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Joey').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Ross').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Monica').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Phoebe').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
     }
-    if (preguntaAleatoria <= 100 && preguntaAleatoria >= 80) {
+    if (preguntaAleatoria <= 100 && preguntaAleatoria >= 81) {
         document.getElementById('Monica').addEventListener('click', function () {
             console.log("Respuesta correcta")
             preguntaAleatoria = 0
             console.log(preguntaAleatoria)
+            array.pop(0)
+            remover()
             Jugar()
-            borrar()
+            
         })
         document.getElementById('Rachel').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Joey').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Chandler').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Ross').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Phoebe').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
     }
-    if (preguntaAleatoria <= 120 && preguntaAleatoria >= 100) {
+    if (preguntaAleatoria <= 120 && preguntaAleatoria >= 101) {
         document.getElementById('Phoebe').addEventListener('click', function () {
             console.log("Respuesta correcta")
             preguntaAleatoria = 0
             console.log(preguntaAleatoria)
+            array.pop(0)
+            remover()
             Jugar()
-            borrar()
+            
         })
         document.getElementById('Rachel').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Joey').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Chandler').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Monica').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
         document.getElementById('Ross').addEventListener('click', function () {
             console.log("Respuesta incorrecta")
-            borrar()
+            
         })
     }
     array.pop(0)
@@ -437,11 +339,10 @@ function pregunta(array, preguntaAleatoria, opcion, opciones) {
         var p = document.createElement("p")
         p.append(array)
         espacio.append(p)
-        console.log(espacio)
+        console.log(p)
     }
 }
-
-function borrar() {
-    var p = $('p')
-    console.log(p)
+function remover() {
+    var texto = $('p')
+    texto.hide()
 }
